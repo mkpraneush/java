b7a8ae3c0f5aff4a7266136f323d044c3c119d98
mkpraneush <mkpraneush3.14@gmail.com>
2025-10-01 19:26:51 +0530

    new upadated file

---
+++ operation.java
@@ -0,0 +1,11 @@
+public class operation {
+    public static void main(String[]args){
+        int a =4;
+        int b =8;
+        
+        float g = (float)a/(float)b;
+        
+        System .out. println(g);
+    }
+    
+}
