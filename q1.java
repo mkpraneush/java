5b97a1c8934652ca25e3a6505f9e852a09b7e227
mkpraneush <mkpraneush3.14@gmail.com>
2025-10-05 21:02:13 +0530

    updated files

---
+++ q1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class q1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        int str2 =sc.nextInt();
+        System.out.println("My name is"+str);
+        System.out.println("My age is"+str2);
+        sc.close();
+    }
+}
